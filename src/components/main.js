856edd0305352cda0e5455f3c1fccfadad4493c8
Maushmi <maushmi.work@gmail.com>
2024-07-28 20:19:00 +0100

    add file

---
+++ src/components/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:093924bb785772a7aac7e4c12c1c97459a19f9c2f601af257fd87666938457ec
+size 9439
---
+++ src/layouts/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a78a2fb0b4b9a7d18753601e5b1f2c33009f70ab279fc1e4af99975aed29fbba
+size 9423
